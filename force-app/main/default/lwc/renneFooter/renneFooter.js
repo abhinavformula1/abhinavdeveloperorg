cecc59e6ed8475562d0d9b10e6e1e3f48c077a73
abhinavformula1 <abhinavformula1@gmail.com>
2023-06-27 19:41:30 +0530

    commit all

---
+++ force-app/main/default/lwc/renneFooter/renneFooter.js
@@ -0,0 +1,5 @@
+import { LightningElement, track } from 'lwc';
+
+export default class RenneFooter extends LightningElement {
+    @track footerMessage = '© 2023 Salesforce, Inc. All rights reserved.';
+}
