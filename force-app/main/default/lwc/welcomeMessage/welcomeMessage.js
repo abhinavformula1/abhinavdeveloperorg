5f338d93fd92a2bcc7266508e14cbca0cb01dda3
abhinavformula1 <abhinavformula1@gmail.com>
2023-06-27 19:43:42 +0530

    commit all

---
+++ force-app/main/default/lwc/welcomeMessage/welcomeMessage.js
@@ -0,0 +1,5 @@
+import { LightningElement, track } from 'lwc';
+
+export default class WelcomeMessage extends LightningElement {
+  @track welcomeMessage = 'Welcome to the Renne Onbarding Applicaiton!';
+}
